8efb1c419af9dc3f2172f51aed6e995c9383ebb9
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-06 10:01:08 +0100

    Join

---
+++ server/11-join.js
@@ -0,0 +1,7 @@
+const elements = ['Fire', 'Air', 'Water'];
+const title = 'Curso de Manipulación de Arrays en JavaScript';
+const rta = elements.join('--');
+const finalUrl = title.split(' ').join('-').toLowerCase();
+
+console.log(rta)
+console.log(finalUrl)
